616f4134182f7b51197ee873683661991cd919dc
FlashChaser <ro10bach@gmail.com>
2019-05-24 08:39:19 +1000

    complete_unfinished_zbattles() fix
    Should determine the correct level cleared now
    
    Co-Authored-By: Dominic Codespoti <thed24@users.noreply.github.com>

--- commands.py
+++ commands.py
@@ -2333,13 +2333,32 @@ def complete_unfinished_zbattles(kagi = False):
             else:
                 url = 'http://ishin-production.aktsk.jp/z_battles/'+str(event['id'])+'/rankings'   
             r = requests.get(url, headers=headers)
-            
+
             # Determine the current stage
             if len(r.json()['friends']) == 0:
                 level = 1
             else:
-                level = int(r.json()['friends'][0]['max_clear_level']) + 1
-            
+                # Find user_id in response and selects the max_clear_level
+                i = 0
+                tempheaders = {
+                        'User-Agent': 'Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0',
+                        'Accept': '*/*',
+                        'Authorization': packet.mac('GET', '/user'),
+                        'Content-type': 'application/json',
+                        'X-Platform': config.platform,
+                        'X-AssetVersion': '////',
+                        'X-DatabaseVersion': '////',
+                        'X-ClientVersion': '////',
+                    }
+                if config.client == 'global':
+                    tempurl = 'https://ishin-global.aktsk.com/user'
+                else:
+                    tempurl = 'http://ishin-production.aktsk.jp/user'
+                    tempr = requests.get(tempurl, headers=tempheaders)
+                for names in r.json()['friends']:
+                    if str(r.json()['friends'][i]['user_id']) == str(tempr.json()['user']['id']):
+                        level = int(r.json()['friends'][i]['max_clear_level']) + 1
+                    i = i + 1
             # Stop at level 30 !! This may not work for all zbattle e.g kid gohan
             while level < 31:
                 ##Get supporters
