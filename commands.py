2cf1ae89bea7faa24c41a3686f697ea242ebd894
PeterPlanlos <peet.planlos@gmail.com>
2019-06-06 08:29:46 +0200

    Added Command supporter
    Ability to change the Support Unit - command change_supporter() implemented.
    GUI and syntax adopted from change_team()

--- commands.py
+++ commands.py
@@ -1091,7 +1091,353 @@ def accept_gifts():
         print(Fore.GREEN + Style.BRIGHT +'Gifts Accepted...')
     else:
         print(r.json())
+####################################################################
+def change_supporter():
+    # Needs to have translation properly implemented!
+    
+    ###Get user cards
+    print(Fore.CYAN + Style.BRIGHT + 'Fetching user cards...')
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0',
+        'Accept': '*/*',
+        'Authorization': packet.mac('GET', '/cards'),
+        'Content-type': 'application/json',
+        'X-Language': 'en',
+        'X-Platform': config.platform,
+        'X-AssetVersion': '////',
+        'X-DatabaseVersion': '////',
+        'X-ClientVersion': '////',
+        }
+    if config.client == 'global':
+        url = 'https://ishin-global.aktsk.com/cards'
+    else:
+        url = 'http://ishin-production.aktsk.jp/cards'
+    r = requests.get(url, headers=headers)
+    master_cards = r.json()['cards']
+    print(Fore.GREEN + Style.BRIGHT + 'Done...')
 
+    ###Sort user cards into a list of dictionaries with attributes
+    print(Fore.CYAN + Style.BRIGHT + 'Fetching card attributes...')
+    card_list = []
+    for card in master_cards:
+        ###Get card collection object from database 
+        try:
+            config.Model.set_connection_resolver(config.db_glb)
+            db_card = config.Cards.find_or_fail(card['card_id'])
+        except:
+            config.Model.set_connection_resolver(config.db_jp)
+            db_card = config.Cards.find_or_fail(card['card_id'])
+        #db_card = config.Cards.where('id','=',card['card_id']).first()
+
+        ###Get card rarity
+        if db_card.rarity == 0:
+            rarity = 'N'
+        elif db_card.rarity == 1:
+            rarity = 'R'
+        elif db_card.rarity == 2:
+            rarity = 'SR'
+        elif db_card.rarity == 3:
+            rarity ='SSR'
+        elif db_card.rarity == 4:
+            rarity = 'UR'
+        elif db_card.rarity == 5:
+            rarity = 'LR'
+        ###Get card Type
+        if str(db_card.element)[-1] == '0':
+            type ='[AGL] '
+        elif str(db_card.element)[-1] == '1':
+            type ='[TEQ] '
+        elif str(db_card.element)[-1] == '2':
+            type ='[INT] '
+        elif str(db_card.element)[-1] == '3':
+            type ='[STR] '
+        elif str(db_card.element)[-1] == '4':
+            type ='[PHY] '
+        ###Get card categories list
+        categories = []
+        #Get category id's given card id
+        card_card_categories = config.CardCardCategories.where(
+                               'card_id','=',db_card.id).get()
+                
+        try:
+            for category in card_card_categories:
+                try:
+                    config.Model.set_connection_resolver(config.db_glb)
+                    categories.append(config.CardCategories.find(
+                                  category.card_category_id).name)
+                except:
+                    config.Model.set_connection_resolver(config.db_jp)
+                    categories.append(config.CardCategories.find(
+                                  category.card_category_id).name)
+        except:
+            None
+        ###Get card link_skills list
+        link_skills = []
+        try:
+            config.Model.set_connection_resolver(config.db_glb)
+            link_skills.append(config.LinkSkills.find(db_card.link_skill1_id).name)
+        except AttributeError:
+            try:
+                config.Model.set_connection_resolver(config.db_jp)
+                link_skills.append(config.LinkSkills.find(db_card.link_skill1_id).name)
+            except:
+                None
+        except:
+            None
+        try:
+            config.Model.set_connection_resolver(config.db_glb)
+            link_skills.append(config.LinkSkills.find(db_card.link_skill2_id).name)
+        except AttributeError:
+            try:
+                config.Model.set_connection_resolver(config.db_jp)
+                link_skills.append(config.LinkSkills.find(db_card.link_skill2_id).name)
+            except:
+                None
+        except:
+            None
+        try:
+            config.Model.set_connection_resolver(config.db_glb)
+            link_skills.append(config.LinkSkills.find(db_card.link_skill3_id).name)
+        except AttributeError:
+            try:
+                config.Model.set_connection_resolver(config.db_jp)
+                link_skills.append(config.LinkSkills.find(db_card.link_skill3_id).name)
+            except:
+                None
+        except:
+            None
+        try:
+            config.Model.set_connection_resolver(config.db_glb)
+            link_skills.append(config.LinkSkills.find(db_card.link_skill4_id).name)
+        except AttributeError:
+            try:
+                config.Model.set_connection_resolver(config.db_jp)
+                link_skills.append(config.LinkSkills.find(db_card.link_skill4_id).name)
+            except:
+                None
+        except:
+            None
+        try:
+            config.Model.set_connection_resolver(config.db_glb)
+            link_skills.append(config.LinkSkills.find(db_card.link_skill5_id).name)
+        except AttributeError:
+            try:
+                config.Model.set_connection_resolver(config.db_jp)
+                link_skills.append(config.LinkSkills.find(db_card.link_skill5_id).name)
+            except:
+                None
+        except:
+            None
+        try:
+            config.Model.set_connection_resolver(config.db_glb)
+            link_skills.append(config.LinkSkills.find(db_card.link_skill6_id).name)
+        except AttributeError:
+            try:
+                config.Model.set_connection_resolver(config.db_jp)
+                link_skills.append(config.LinkSkills.find(db_card.link_skill6_id).name)
+            except:
+                None
+        except:
+            None
+        try:
+            config.Model.set_connection_resolver(config.db_glb)
+            link_skills.append(config.LinkSkills.find(db_card.link_skill7_id).name)
+        except AttributeError:
+            try:
+                config.Model.set_connection_resolver(config.db_jp)
+                link_skills.append(config.LinkSkills.find(db_card.link_skill7_id).name)
+            except:
+                None
+        except:
+            None
+
+        dict = {
+                'ID': db_card.id,
+                'Rarity': rarity,
+                'Name': db_card.name,
+                'Type': type,
+                'Cost': db_card.cost,
+                'Hercule': db_card.is_selling_only,
+                'HP': db_card.hp_init,
+                'Categories':categories,
+                'Links':link_skills,
+                'UniqueID': card['id']
+                }
+        card_list.append(dict)
+    print(Fore.GREEN + Style.BRIGHT + "Done...")
+
+    ###Sort cards
+    print(Fore.CYAN + Style.BRIGHT + "Sorting cards...")
+    card_list = sorted(card_list, key=lambda k: k['Name'])
+    card_list = sorted(card_list, key=lambda k: k['Rarity'])
+    card_list = sorted(card_list, key=lambda k: k['Cost'])
+    print(Fore.GREEN + Style.BRIGHT + "Done...")
+    ###Define cards to display
+    cards_to_display_dicts = []
+    cards_to_display = []
+    #Take cards in card_list that aren't hercule statues or kais?
+    for char in card_list:
+        if char['Hercule'] != 1 and char['HP'] > 5:
+            cards_to_display_dicts.append(char)
+            cards_to_display.append(char['Type'] + char['Rarity']+ ' ' +char['Name'] + ' | ' + str(char['ID']) + ' | '+ str(char['UniqueID']))
+
+    ###Define links to display
+    links_master = []
+    config.Model.set_connection_resolver(config.db_jp)
+    for link in config.LinkSkills.all():
+        links_master.append(link.name)
+        try:
+            config.Model.set_connection_resolver(config.db_glb)
+            links_master.append(config.LinkSkills.find_or_fail(link.id).name)
+        except:
+            config.Model.set_connection_resolver(config.db_jp)
+            links_master.append(config.LinkSkills.find_or_fail(link.id).name)
+    
+    links_to_display = sorted(links_master)
+
+    ###Define categories to display
+    categories_master = []
+    config.Model.set_connection_resolver(config.db_jp)
+    for category in config.CardCategories.all():
+        try:
+            config.Model.set_connection_resolver(config.db_glb)
+            categories_master.append(config.CardCategories.find_or_fail(category.id).name)
+        except:
+            config.Model.set_connection_resolver(config.db_jp)
+            categories_master.append(config.CardCategories.find_or_fail(category.id).name)
+    
+    categories_to_display = sorted(categories_master)
+    
+    ###Define window layout
+
+    col1 = [[sg.Listbox(values=(cards_to_display),size = (30,20),key='CARDS')],
+            [sg.Listbox(values=([]),size = (30,6),key = 'CARDS_CHOSEN')],
+            [sg.Button(button_text = 'Set as Supporter',key='choose_card')]]
+
+    col2 = [[sg.Listbox(values=(sorted(categories_to_display)),size = (25,20),key = 'CATEGORIES')],
+            [sg.Listbox(values=([]),size = (25,6),key = 'CATEGORIES_CHOSEN')],
+            [sg.Button(button_text ='Choose Categories',key='choose_categories'),
+             sg.Button(button_text ='Clear Categories',key='clear_categories')]]
+
+    col3 = [[sg.Listbox(values=(sorted(links_to_display)),size = (25,20),key='LINKS')],
+            [sg.Listbox(values=([]),size = (25,6),key = 'LINKS_CHOSEN')],
+            [sg.Button(button_text ='Choose Links',key='choose_links'),
+             sg.Button(button_text ='Clear Links',key='clear_links')]]
+
+    layout = [[sg.Column(col1),sg.Column(col2),sg.Column(col3)]]
+    window = sg.Window('Deck Update',grab_anywhere=True,keep_on_top = True).Layout(layout)
+    
+
+    ###Begin window loop
+    chosen_links = []
+    chosen_categories = []
+
+    ###
+    chosen_cards_ids = []
+    chosen_cards_unique_ids = []
+    chosen_cards_names = []
+    chosen_cards_to_display = []
+
+    while len(chosen_cards_ids) < 1:
+        event,values = window.Read()
+
+        if event == None:
+            return 0
+        
+        if event == 'choose_card':
+            if len(values['CARDS']) < 1:
+                continue
+            #Get ID of chosen card to send to bandai
+            chosen_line = values['CARDS'][0]
+            char_name,char_id,char_unique_id = chosen_line.split(' | ')
+            chosen_cards_ids.append(int(char_id))
+            chosen_cards_unique_ids.append(int(char_unique_id))
+            try:
+                config.Model.set_connection_resolver(config.db_glb)
+                chosen_cards_names.append(config.Cards.find(char_id).name)
+            except:
+                config.Model.set_connection_resolver(config.db_jp)
+                chosen_cards_names.append(config.Cards.find(char_id).name)
+
+
+            #Chosen cards to display in lower box
+            chosen_cards_to_display.append(chosen_line)
+
+        if event == 'choose_categories':
+            for category in values['CATEGORIES']:
+                chosen_categories.append(category)
+                categories_to_display.remove(category)
+
+        if event == 'clear_categories':
+            categories_to_display.extend(chosen_categories)
+            chosen_categories[:] = []
+            categories_to_display = sorted(categories_to_display)
+            
+
+        if event == 'choose_links':
+            for link in values['LINKS']:
+                chosen_links.append(link)
+                links_to_display.remove(link)
+
+        if event == 'clear_links':
+            links_to_display.extend(chosen_links)
+            chosen_links[:] = []
+            links_to_display = sorted(links_to_display)
+            break
+
+        ###Re-populate cards to display, checking filter criteria
+        cards_to_display[:] = []
+        for char in cards_to_display_dicts:
+            if char['Name'] in chosen_cards_names:
+                continue
+
+            if len(list(set(chosen_links) & set(char['Links']))) != len(chosen_links):
+                #print("List intersection")
+                continue
+
+            if len(list(set(chosen_categories) & set(char['Categories']))) != len(chosen_categories):
+                #print("Category intersectino")
+                continue
+
+            cards_to_display.append(char['Type'] + char['Rarity']+ ' ' +char['Name'] + ' | ' + str(char['ID']) + ' | '+ str(char['UniqueID']))
+
+
+        ###Update window elements
+        window.FindElement('CARDS').Update(values=cards_to_display)
+        window.FindElement('CARDS_CHOSEN').Update(values=chosen_cards_to_display)
+        window.FindElement('CATEGORIES').Update(values=categories_to_display)
+        window.FindElement('CATEGORIES_CHOSEN').Update(values=chosen_categories)
+        window.FindElement('LINKS').Update(values=links_to_display)
+        window.FindElement('LINKS_CHOSEN').Update(values=chosen_links)
+
+    window.Close()
+    ###Send selected supporter to bandai
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0',
+        'Accept': '*/*',
+        'Authorization': packet.mac('PUT', '/support_leaders'),
+        'Content-type': 'application/json',
+        'X-Platform': config.platform,
+        'X-AssetVersion': '////',
+        'X-DatabaseVersion': '////',
+        'X-ClientVersion': '////',
+        }
+    if config.client == 'global':
+        url = 'https://ishin-global.aktsk.com/support_leaders'
+    else:
+        url = 'http://ishin-production.aktsk.jp/support_leaders'
+    #print(chosen_cards_unique_ids)
+    data = {'support_leader_ids':chosen_cards_unique_ids}
+    #print(data)
+    r = requests.put(url, data = json.dumps(data),headers = headers)
+    if 'error' in r.json():
+        print(Fore.RED + Style.BRIGHT+str(r.json()))
+    else:
+        #print(r.json())
+        print(chosen_cards_names)
+        print(Fore.GREEN + Style.BRIGHT+"Supporter updated!")
+
+    return 0
 ####################################################################
 def change_team():
     # Needs to have translation properly implemented!
@@ -2282,6 +2628,8 @@ def user_command_executor(command):
         list_cards()
     elif command == 'team':
         change_team()
+    elif command == 'supporter':
+        change_supporter()   
     elif command == 'deck':
         config.deck = int(input('Enter a deck number to use: '))
     elif command == 'transfer':
